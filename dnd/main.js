f76c711451a2f78cf0be9f09643a3d0973dc0ad1
vitusverden <37027595+vitusverden@users.noreply.github.com>
2020-09-04 19:38:20 +0200

    Diverse debugging stuff

--- dnd/main.js
+++ dnd/main.js
@@ -2,8 +2,11 @@ function randomIntFromInterval(min, max) { // min and max included
 	return Math.floor(Math.random() * (max - min + 1) + min);
   }
 function roll(times) {
+	console.clear()
+	console.time("total")
 	document.getElementById("output").innerHTML = ""
 	for (let i = 1; i <= times ; i++) {
+		console.time("day" + i)
 		var listoftotals = []
 		for (let o = 1; o <= 3; o++) {
 			var person1roll = randomIntFromInterval(1,20)
@@ -19,10 +22,12 @@ function roll(times) {
 			} else {
 				person2roll += Number(document.getElementById("vagt" + o + "1Perception").value)
 			}
-			console.log("dag " + i + "vagt " + o + " person 2 modifier: " + Number(document.getElementById("vagt" + o + "2Perception").value))
+			//console.log("dag " + i + "vagt " + o + " person 2 modifier: " + Number(document.getElementById("vagt" + o + "2Perception").value))
 
 			listoftotals.push("(" + person1roll + ", " + person2roll +  ")")
 		}
 		document.getElementById("output").innerHTML += "<p> Dag " + i + ": " + listoftotals[0] + ", " + listoftotals[1] + ", " + listoftotals[2] + "</p>"
+		console.timeEnd("day" + i)
 	}
+	console.timeEnd("total")
 }
